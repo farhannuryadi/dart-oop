e827f5741a28e57a10757b21e9abd61d087c4d0b
Farhan Nuryadi <farhannuryadi6@gmail.com>
2023-01-11 23:56:31 +0700

    initializer list

---
+++ bin/initializer_list.dart
@@ -0,0 +1,26 @@
+class Customer {
+
+  String firstName = '';
+  String lastName = '';
+  String fullName = '';
+
+  Customer(this.fullName)
+      : firstName = fullName.split(' ')[0],
+        lastName = fullName.split(' ')[1] {
+
+    print('Create New Customer');
+
+  }
+
+}
+
+
+
+void main(){
+
+  var customer = Customer("Farhan Nuryadi");
+  print(customer.fullName);
+  print(customer.firstName);
+  print(customer.lastName);
+
+}
