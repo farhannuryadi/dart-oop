0912509144eebd9a233754efbbc63ec5d1a42ae1
Farhan Nuryadi <farhannuryadi6@gmail.com>
2023-01-19 00:11:48 +0700

    static field

---
+++ bin/static_field.dart
@@ -0,0 +1,13 @@
+class Application{
+
+  static final String name = 'Belajar Dart OOP';
+  static final String author = 'Farhan Nuryadi';
+
+}
+
+void main(){
+
+  print(Application.name);
+  print(Application.author);
+
+}
