5b63df5c3c1133a019c6588cd61bacf0fad8477b
Farhan Nuryadi <farhannuryadi6@gmail.com>
2023-01-18 22:36:07 +0700

    toString

---
+++ bin/data/customer.dart
@@ -0,0 +1,9 @@
+class Customer{
+
+  String? name;
+  String? age;
+  String? address;
+
+  String toString() => "Customer{\"name\" : $name, \"age\" : $age, \"address\" : $address}";
+
+}
---
+++ bin/tostring.dart
@@ -0,0 +1,11 @@
+import 'data/customer.dart';
+
+void main(){
+
+  var customer = Customer();
+  customer.name = "Farhan";
+  customer.age = "17";
+  customer.address = "Depok";
+  print(customer);
+
+}
