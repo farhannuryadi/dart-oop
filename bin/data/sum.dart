3149cbe8d7febc22d999583c2d8c295b65a31603
Farhan Nuryadi <farhannuryadi6@gmail.com>
2023-01-18 23:51:24 +0700

    typedef

---
+++ bin/data/sum.dart
@@ -0,0 +1,13 @@
+class Sum{
+
+  int first;
+  int last;
+
+  Sum(this.first, this.last);
+
+  int hitung() => first + last;
+
+}
+
+typedef Jumlah = Sum;
+typedef Total = Sum;
---
+++ bin/typedef.dart
@@ -0,0 +1,11 @@
+import 'data/sum.dart';
+
+void main(){
+
+  var jumlah = Jumlah(4,5);
+  print(jumlah.hitung());
+
+  var total = Total(4,5);
+  print(total.hitung());
+
+}
