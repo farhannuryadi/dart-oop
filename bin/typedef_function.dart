38413535e41cd93dd8596cc3b912f394553d606c
Farhan Nuryadi <farhannuryadi6@gmail.com>
2023-01-18 23:54:15 +0700

    typedef function

---
+++ bin/typedef_function.dart
@@ -0,0 +1,11 @@
+typedef Filter = String Function(String);
+
+void sayHello(String name, Filter filter){
+  print('Hello ${filter(name)}');
+}
+
+void main(){
+
+  sayHello('Hann', (value) => value.toUpperCase());
+
+}
